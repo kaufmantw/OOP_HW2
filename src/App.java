4c251332bb650bdda00586a0d2ba3dd22afd6ec3
kaufmantw <kaufmantw@jacks.sfasu.edu>
2022-10-01 21:34:12 -0500

    initial commit
    imported libraries and created myTicTacToe

---
+++ src/App.java
@@ -0,0 +1,19 @@
+import cs331Helper.cs331TicTacToeFrame;
+import cs331Helper.cs331TicTacToeGame;
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class App extends Application{
+
+    public void start(Stage stage){
+
+        cs331TicTacToeFrame frame = new cs331TicTacToeFrame(stage);
+     
+        // Add the game here to the frame
+        
+        
+    }
+    public static void main(String[] args) throws Exception {
+        launch(args);
+    }
+}
---
+++ src/myTicTacToe.java
@@ -0,0 +1,21 @@
+public class Game implements cs331TicTacToeGame{
+    public Game(){
+
+    }
+
+    public void invalidSquareChosen(int row, int col){
+
+    }
+
+    public void noWinner(){
+
+    }
+
+    public void playerWins(){
+
+    }
+
+    public void restartGame(){
+        
+    }
+}
