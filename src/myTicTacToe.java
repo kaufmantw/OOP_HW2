e95456792be2e9f9b32a2e6378fec46fa4ece06c
kaufmantw <kaufmantw@jacks.sfasu.edu>
2022-10-07 10:45:23 -0500

    step 2 and 3
    Did steps 2 and 3 of the homework, created base game. no player yet

--- src/myTicTacToe.java
+++ src/myTicTacToe.java
@@ -1,6 +1,15 @@
-public class Game implements cs331TicTacToeGame{
-    public Game(){
+import cs331Helper.cs331TicTacToeBoard;
+import cs331Helper.cs331TicTacToeController;
+import cs331Helper.cs331TicTacToeGame;
 
+public class myTicTacToe implements cs331TicTacToeGame{
+    private cs331TicTacToeBoard board;
+    private cs331TicTacToeController control;
+
+    public myTicTacToe(){
+        this.board = new cs331TicTacToeBoard();
+        this.control = new cs331TicTacToeController();
+        restartGame();
     }
 
     public void invalidSquareChosen(int row, int col){
@@ -16,6 +25,6 @@ public class Game implements cs331TicTacToeGame{
     }
 
     public void restartGame(){
-        
+        this.control.setControllerMessage("Select a square to start playing!");
     }
 }
